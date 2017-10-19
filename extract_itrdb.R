be47d2967055c8a2065c4c34b032a6f82a1f1c16
crollinson <crollinson@gmail.com>
2017-10-19 15:13:11 -0500

    ITRDB extraction updates
    Work with JSON format using jsonlite makes life SO MUCH EASIER!
    
    This makes for a cleaner code and a TON easier to find useful metadata.
     This also means we don’t need to make a new data frame with the
    metadata since we can convert the JSON info to a data frame.
    
    Next steps will be adding support for non-square bounding box &
    converting to a full function

--- extract_itrdb.R
+++ extract_itrdb.R
@@ -2,15 +2,20 @@ library(curl)
 library(XML)
 library(stringr)
 library(raster)
+library(RCurl)
+library(jsonlite)
 
 dat.out <- "~/Desktop/ITRDB_test"
 if(!dir.exists(dat.out)) dir.create(dat.out, recursive=T)
 
+# download.all <- c("Chronology", "Raw Measurements", "Correlation Stats")
+download.types <- c("Chronology", "Raw Measurements")
 
 us <- getData("GADM", country="USA", level=1)
 us$NAME_1 <- as.factor(us$NAME_1)
 states <- c("Maine", "Vermont", "Massachusetts", "New Hampshire", "Vermont", "Connecticut", "Rhode Island", "New York", "Pennsylvania", "New Jersey")
-borders <- us[us$NAME_1 %in% c("New York"),]
+# borders <- us[us$NAME_1 %in% c("New York"),]
+borders <- us[us$NAME_1 %in% states,]
 
 xmin = xmin(borders)
 xmax = xmax(borders)
@@ -20,78 +25,63 @@ ymax = ymax(borders)
 itrdb.base <- paste0("https://www.ncdc.noaa.gov/paleo-search/study/search.json?headersOnly=true&dataPublisher=NOAA&dataTypeId=18&minLat=",ymin, "&maxLat=", ymax,
                      "&minLon=", xmin, "&maxLon=", xmax)
 
-noaa.meta <- readLines(itrdb.base)
+noaa.meta <- RCurl::getURL(itrdb.base)
+noaa.meta <- jsonlite::fromJSON(noaa.meta)$study
 
-meta.parse <- str_split(noaa.meta, ",")[[1]]
-# meta.parse[1:30]
-noaa.id <- grep("NOAAStudyId", meta.parse) # get our indices for our URLS we'll query
+for(i in 1:ncol(noaa.meta)){
+  noaa.meta[,i] <- as.factor(noaa.meta[,i])
+}
+summary(noaa.meta)
 
-itrdb.metadata <- data.frame(ID=NA, author=NA, site=NA, species=NA, lat=NA, lon=NA, elev=NA, ITRDB.id=NA, yr.min=NA, yr.max=NA)
+# Set up where we're going to store everything
+# itrdb.metadata <- data.frame(ID.study=NA, investigators=NA, studyCode=NA, species=NA, lat=NA, lon=NA, elev=NA, ITRDB.id=NA, yr.min=NA, yr.max=NA)
 
 # Working with one site for now; 
 # # will need to add loop here in future
-pb <- txtProgressBar(min=0, max=length(noaa.id), style=3)
-for(i in 1:length(noaa.id)){
-  site.id <- str_split(meta.parse[noaa.id[i]], '":\"')[[1]][2]
-  site.id <- substr(site.id, 1, nchar(site.id)-1)
-
-  # Store the Site ID
-  itrdb.metadata[i,"ID"     ] <- site.id
+pb <- txtProgressBar(min=0, max=nrow(noaa.meta), style=3)
+for(i in 1:nrow(noaa.meta)){
+  id.xml <- noaa.meta[i, "xmlId"]
   
   # Download and save the XML document with all of the metadata for the site
   # We can extract info from this site for our use later
-  site.xml <- xmlParse(readLines(paste0("https://www1.ncdc.noaa.gov/pub/data/metadata/published/paleo/dif/xml/noaa-tree-",site.id,".xml")))
+  site.dat <- jsonlite::fromJSON(paste0("https://www.ncdc.noaa.gov/paleo-search/study/search.json?xmlId=", id.xml))$study
+  summary(site.dat)
   
-  # Parsing the XML file into something easier to use
-  site.xml2 <- xmlToList(site.xml)
-  summary(site.xml2)
+  # itrdb.metadata[i,"investigators" ]  <- site.dat$investigators
+  noaa.meta[i ,"studyCode"] <- site.dat$studyCode
+  noaa.meta[i, "siteName" ] <- site.dat$site[[1]]$siteName
+  noaa.meta[i, "Latitude" ] <- as.numeric(site.dat$site[[1]]$geo$geometry$coordinates[[1]][1])
+  noaa.meta[i, "Longitude"] <- as.numeric(site.dat$site[[1]]$geo$geometry$coordinates[[1]][2])
+  noaa.meta[i, "Elevation"] <- mean(as.numeric(site.dat$site[[1]]$geo$properties[,c("minElevationMeters", "maxElevationMeters")]))
+  
+  # Extract the species info where available
+  if(length(site.dat$site[[1]]$paleoData[[1]]$species[[1]])>0){
+    noaa.meta[i,"species.code"   ]  <- site.dat$site[[1]]$paleoData[[1]]$species[[1]]$speciesCode
+    noaa.meta[i,"species.name"   ]  <- site.dat$site[[1]]$paleoData[[1]]$species[[1]]$scientificName
+  }
+  
+  
 
-  title <- str_split(site.xml2$Entry_Title, "- ")[[1]]
-  itrdb.metadata[i,"author" ]  <- title[1]
-  itrdb.metadata[i,"site"   ]  <- title[2]
-  itrdb.metadata[i,"spieces"]  <- title[3]
-
-  itrdb.id <- substr(title[4], nchar(title[4])-4, nchar(title[4]))
-  itrdb.metadata[i,"ITRDB.id"] <- itrdb.id
-  saveXML(site.xml, file=file.path(dat.out, paste0("Metadata_", itrdb.id, ".xml")))
-  
-  # Extract & Store spatial info
-  lat1 <- as.numeric(site.xml2$Spatial_Coverage$Southernmost_Latitude)
-  lat2 <- as.numeric(site.xml2$Spatial_Coverage$Northernmost_Latitude)
-  lon1 <- as.numeric(site.xml2$Spatial_Coverage$Westernmost_Longitude)
-  lon2 <- as.numeric(site.xml2$Spatial_Coverage$Easternmost_Longitude)
-  elv1 <- as.numeric(site.xml2$Spatial_Coverage$Minimum_Altitude)
-  elv2 <- as.numeric(site.xml2$Spatial_Coverage$Maximum_Altitude)
-  itrdb.metadata[i,"lat"    ] <- mean(lat1, lat2)
-  itrdb.metadata[i,"lon"    ] <- mean(lon1, lon2)
-  itrdb.metadata[i,"elev"   ] <- ifelse(length(elv1)>0, mean(elv1, elv2), NA)
-  
   # Extract for the start/end dates
-  itrdb.metadata[i,"yr.min"] <- site.xml2$Paleo_Temporal_Coverage$Paleo_Start_Date
-  itrdb.metadata[i,"yr.max"] <- site.xml2$Paleo_Temporal_Coverage$Paleo_Stop_Date
+  noaa.meta[i,"yr.min"] <- as.numeric(site.dat$earliestYearCE)
+  noaa.meta[i,"yr.max"] <- as.numeric(site.dat$mostRecentYearCE)
   
   # Going through the URLs to find a .rwl file
-  data.urls <- which(names(site.xml2)=="Related_URL")
+  site.dat <- site.dat$site[[1]]$paleoData[[1]]$dataFile[[1]]
   
-  # url.rwl <- list()
-  for(j in 1:length(data.urls)){
-    url.temp <- site.xml2[[data.urls[j]]]
-    url.ind <- which(names(url.temp)=="URL")
-    for(z in 1:length(url.ind)){
-      fname <- str_split(url.temp[url.ind[z]], "/")[[1]]
-      fname <- fname[length(fname)]
-      
-      url.ext <- substr(fname, nchar(fname)-3, nchar(fname))
-      if(url.ext %in% c(".rwl", ".crn")){ 
-        download.file(url.temp[url.ind[z]]$URL, file.path(dat.out, fname), quiet=T)
-      } # End saving file
-    } # End looping through URLs
+  data.urls <- site.dat[site.dat$urlDescription %in% download.types,]
+  
+  # Make a note of how many files were downloaded
+  noaa.meta[i,"files.download"] <- nrow(data.urls)
+  
+  if(nrow(data.urls)==0) next 
+  for(j in 1:nrow(data.urls)){
+    download.file(data.urls$fileUrl[j], file.path(dat.out, data.urls$linkText[j]), quiet=T)
   } # End looping thorugh URL types
   
   setTxtProgressBar(pb, i)
 }
+noaa.meta$species.code <- as.factor(noaa.meta$species.code)
+noaa.meta$species.name <- as.factor(noaa.meta$species.code)
+summary(noaa.meta)
 
-itrdb.metadata$ITRDB.id <- as.factor(itrdb.metadata$ITRDB.id)
-itrdb.metadata$species <- as.factor(itrdb.metadata$species)
-# itrdb.metadata$species <- as.factor(itrdb.metadata$speices)
-summary(itrdb.metadata)
