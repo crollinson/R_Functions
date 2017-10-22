1f152ba0aa0de66b72b9979abee8d4e78cd56a29
crollinson <crollinson@gmail.com>
2017-10-22 09:51:36 -0500

    a couple ITRDB workflow cleanups

--- ITRDB_extract_workflow.R
+++ ITRDB_extract_workflow.R
@@ -1,11 +1,18 @@
 dat.out <- "~/Desktop/ITRDB_test"
 
-us <- getData("GADM", country="USA", level=1)
+# ----------------------
+# Example of pulling a state shapefile
+# ----------------------
+us <- raster::getData("GADM", country="USA", level=1)
 us$NAME_1 <- as.factor(us$NAME_1)
 states <- c("Pennsylvania")
 # borders <- us[us$NAME_1 %in% c("New York"),]
 borders <- us[us$NAME_1 %in% states,]
+# ----------------------
 
+# ----------------------
+# Making a square bounding box if you just want an area
+# ----------------------
 xmin = xmin(borders)
 xmax = xmax(borders)
 ymin = ymin(borders)
@@ -16,5 +23,29 @@ test <- data.frame(x=c(xmin, xmax, xmax, xmin, xmin),
 
 test <- Polygon(test)
 test <- SpatialPolygons(list(Polygons(list(test), ID="a")), proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+# ----------------------
 
-itrdb.out <- extract.itrdb(area.extract=borders, download.types=c("Chronology", "Raw Measurements"), dir.out="~/Desktop/ITRDB_europe")
+
+
+# ----------------------
+# Getting Europe
+# ----------------------
+europe <- rworldmap::getMap()
+europe <- europe[europe$GLOCAF=="Europe" & !is.na(europe$GLOCAF) & europe$TYPE %in% c("Sovereign country", "Country"),]
+europe <- europe[!europe$IMAGE24 %in% c("Middle East", "Greenland"),]
+summary(europe)
+plot(europe)
+
+
+# worldmap2 <- ggplot2::map_data("world")
+# worldmap2$region <- as.factor(worldmap2$region)
+
+# ----------------------
+
+
+
+# ----------------------
+# Doing the actual query
+# ----------------------
+itrdb.out <- extract.itrdb(area.extract=europe, download.types=c("Chronology", "Raw Measurements"), dir.out="~/Desktop/ITRDB_europe")
+# ----------------------
