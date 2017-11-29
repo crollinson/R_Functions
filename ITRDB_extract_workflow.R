4afd0215666df0b40c8022277a1893ba9c58029d
crollinson <crollinson@gmail.com>
2017-11-29 08:06:17 -0600

    add quick plotting step

--- ITRDB_extract_workflow.R
+++ ITRDB_extract_workflow.R
@@ -50,3 +50,9 @@ plot(europe)
 itrdb.out <- extract.itrdb(area.extract=europe, download.types=c("Chronology", "Raw Measurements"), dir.out="~/Desktop/ITRDB_europe")
 write.csv(itrdb.out, "~/Desktop/ITRDB_europe/ITRDB_Europe_metadata.csv", row.names=F)
 # ----------------------
+
+
+itrdb.sp <- SpatialPointsDataFrame(coords=itrdb.out[,c("Longitude", "Latitude")], itrdb.out, proj4string = CRS(projection(europe)))
+
+plot(europe)
+plot(itrdb.sp, add=T, pch=20, col="red", cex=0.5)
